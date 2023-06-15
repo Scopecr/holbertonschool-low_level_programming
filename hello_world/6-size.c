d04957e13ce240239544bafce944e3613ac448f9
Scopecr <wjrcarrion@gmail.com>
2023-06-15 18:33:10 +0000

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -2,10 +2,6 @@
 
 /**
  * main - Entery point
- * float - Datatype for floating point
- * long int - integer
- * long long int - inter
- * char - a point of character
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -16,10 +12,10 @@ int main(void)
 	char d;
 	float f;
 
-	printf("Size of a char:%lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of an int:%lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of an long int:%lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of an long long int:%lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of an long float:%lu byte(s)\n", (unsigned long) sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of an long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an long float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
