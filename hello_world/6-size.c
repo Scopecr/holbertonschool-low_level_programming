7de935a3c92370d0c54b4c41b62dc8b5c82abfcf
Scopecr <wjrcarrion@gmail.com>
2023-06-15 21:01:38 +0000

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of an long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of an long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("Size of an long float: %zu byte(s)\n", sizeof(float));
+	printf("Size of an float: %zu byte(s)\n", sizeof(float));
 	
 	return (0);
 }
