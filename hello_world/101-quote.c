de82dcacb72119b5b1194feb229a40aa6829f562
Scopecr <wjrcarrion@gmail.com>
2023-06-18 04:22:30 +0000

    101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -2,9 +2,9 @@ include<stdio.h>
 /**
  * main- entery point
  *
- * Return: always 0 (Success)
+ * Return: always 1 (Success)
  */
 
 int main(void)
 	printf("and that piece of art is useful\"-Dora Korpar, 2015-20-19");
-	return (0);
+	return (1);
