857379fd35afb2ece9cb3022f5eba63b6fd70405
Scopecr <wjrcarrion@gmail.com>
2023-06-18 04:25:11 +0000

    101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -4,7 +4,9 @@ include<stdio.h>
  *
  * Return: always 1 (Success)
  */
+int main(void) {
+    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, message, sizeof(message) - 1);
+    return 1;
+}
 
-int main(void)
-	printf("and that piece of art is useful\"-Dora Korpar, 2015-20-19");
-	return (1);
