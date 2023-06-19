9a492575c79d9a2e5e2b8d6bfe91faca01bc89a8
Scopecr <wjrcarrion@gmail.com>
2023-06-19 21:02:46 +0000

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ *
+ * int num - variable indicates the increase in value
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
