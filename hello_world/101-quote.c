0f8a9df4eeb084dd6a407831dced79d0c0b6a2ab
Scopecr <wjrcarrion@gmail.com>
2023-06-19 12:50:14 +0000

    101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -5,8 +5,7 @@
  * Return: always 1 (Success)
  */
 int main(void) {
-	fwrite(2,
-			("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",58);
     return (1);
 }
 
