81f13ca402407e6302c20eae6fbad7c3744fd7a5
Scopecr <wjrcarrion@gmail.com>
2023-06-19 12:45:30 +0000

    101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -5,8 +5,8 @@
  * Return: always 1 (Success)
  */
 int main(void) {
-    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, message, sizeof(message) - 1);
-    return 1;
+	write(2,
+			("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+    return (1);
 }
 
