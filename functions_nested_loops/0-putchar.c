31af175602a064243c41378d25ff8e1b67511f01
Scopecr <wjrcarrion@gmail.com>
2023-06-20 17:46:58 +0000

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -8,6 +8,13 @@
  */
 int main(void)
 {
-	_putchar('\n');
+	_putchar("_\n");
+	_putchar("p\n");
+	_putchar("u\n");
+	_putchar("t\n");
+	_putchar("c\n");
+	_putchar("h\n");
+	_putchar("a\n");
+	_putchar("r\n");
 	return (0);
 }
