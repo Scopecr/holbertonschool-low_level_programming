0d108d484260556ff57f00138728fe95e86f84c7
Scopecr <wjrcarrion@gmail.com>
2023-06-20 16:58:02 +0000

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+/**
+ *main - entry point
+ *char message - prints _putchar
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char message[] = "_putchar\n";
+
+	write(STDOUT_FILENO, message, sizeof(message) - 1);
+
+	return (0);
+}
