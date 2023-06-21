4e5e1bb2555c913fc9fb3b38f69ea73853b9d28d
Scopecr <wjrcarrion@gmail.com>
2023-06-21 02:07:59 +0000

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * islower - identifies the lower case ad promps to return1
+ *
+ * Return: 1 if lower case found
+ *
+ * Return: 0 success no lower case
+ */
+
+int main(void)
+	{
+	int _islower(int C)
+
+		if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
+
