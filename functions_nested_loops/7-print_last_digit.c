e7aa8fc98ecff67ef048b02d7f1666e1167ccfe9
Scopecr <wjrcarrion@gmail.com>
2023-06-21 20:18:30 +0000

    7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -6,9 +6,15 @@
  *
  * Return: back to i for the value of the last digit
  */
-int print_last_digit(int i){
-	if (i % 10)
-	{
-		_putchar(i);
-	}
-		return (i);
+int print_last_digit(int i)
+{
+	int last_digit = i % 10;
+
+	if (last_digit < 0)
+		last_digit *= -1;
+
+	_putchar(last_digit - '0');
+
+	return (last_digit);
+}
+
