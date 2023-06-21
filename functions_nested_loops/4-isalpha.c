615eec96e791d4012866fd2c6dd485deb2d950bd
Scopecr <wjrcarrion@gmail.com>
2023-06-21 18:30:19 +0000

    4-isalpha.c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdio.h>
+/**
+ * _isalpha - Checks if a character is alphabetic.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
+ */
+
+int _isalpha(int c)
+{
+	c = _isalpha(c);
+	if (c != 0)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
