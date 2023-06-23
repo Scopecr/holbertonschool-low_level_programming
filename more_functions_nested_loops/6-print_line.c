14483d13c2bc7d78213d8b06276a7794b80da2b3
Scopecr <wjrcarrion@gmail.com>
2023-06-23 23:36:15 +0000

    6-print_line.c

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,16 +1,22 @@
 #include "main.h"
+
 /**
- * print_line - Draws a straight line using the character _.
- * @n: The number of _ characters to be printed.
+ * print_line - prints lines
+ * @n: the number of lines to print
+ *
+ * Return: void
  */
+
 void print_line(int n)
 {
-	int len;
+	int i;
 
 	if (n > 0)
 	{
-		for (len = 0; len < n; len++)
+		for (i = 0; i < n; i++)
+		{
 			_putchar('_');
+		}
 	}
 
 	_putchar('\n');
