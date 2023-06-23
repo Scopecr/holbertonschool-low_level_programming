80f63abce0a37134d299c6b914e2b7d7ae3a361e
Scopecr <wjrcarrion@gmail.com>
2023-06-23 22:50:47 +0000

    3-print_numbers.c

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -7,10 +7,16 @@
 */
 void print_numbers(void)
 {
+
 int number = 48;
-for (number = 48; number < 58; number++)
-{
-_putchar(number);
-}
-_putchar(10);
+
+	for (number = 48; number < 58; number++)
+
+	{
+
+		_putchar(number);
+
+	}
+
+		_putchar(10);
 }
