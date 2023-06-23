c1202ffc62020d545223068f1975b8e9b43bdc58
Scopecr <wjrcarrion@gmail.com>
2023-06-23 22:41:59 +0000

    1-isdigit.c

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,25 @@
 #include "main.h"
-
+/**
+ *_isdigit - identifies if there are digits on the program.
+ *
+ *@C: variable to identify as number
+ *
+ *Returns: 1 if digit found and 0 if no digit
+*/
 int  _isdigit(int c)
 {
-    if (c >= 0 && c <= 9)
+	if (c >= 0 && c <= 10)
 
-    {
-     return (1);
-    }
+	{
 
-    else
-    {
-        return (0);
-    }
+		return (1);
+
+	}
+
+	else
+	{
+
+		return (0);
+
+	}
 }
