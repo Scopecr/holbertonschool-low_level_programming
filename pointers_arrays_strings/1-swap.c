4cafeb953e7a1ac15048f261b0f54849c455651e
Scopecr <wjrcarrion@gmail.com>
2023-06-26 17:04:48 +0000

    1-swap.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *swap_int - swaps the value of two intergers
+ *
+ * @a: interger to swap with b
+ * @b: ineterger to swap with a
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*d = aux;
+}
