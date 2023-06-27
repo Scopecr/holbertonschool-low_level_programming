59fe23e60e15e188e7bb044768057467f3ae9023
Scopecr <wjrcarrion@gmail.com>
2023-06-27 02:30:19 +0000

    5-rev_string.c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * rev_string - puts a string to stdout reversed
+ * @s: the string
+ *
+ * Return: void
+ */
+
 void rev_string(char *s)
 {
 	int len = 0;
