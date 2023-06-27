5e56bd1232b60fc94c20a6c4cba7f7fc4ad5f3e1
Scopecr <wjrcarrion@gmail.com>
2023-06-27 12:46:42 +0000

    5-rev_string.c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -13,14 +13,14 @@ void rev_string(char *s)
 	int len = 0;
 	int i, j;
 
-	while (*(s+len) != '\0')
+	while (*(s + len) != '\0')
 		len++;
 	len--;
 
 	for (i = 0, j = len; i < j; i++, j--)
 	{
 		char temp = *(s + i);
-		*(s + i) = *(s+j);
+		*(s + i) = *(s + j);
 		*(s + j) = temp;
 	}
 }
