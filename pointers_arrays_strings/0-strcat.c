ad91041789b0f8068482d512d3c2bcc61e1c7ce8
Scopecr <wjrcarrion@gmail.com>
2023-06-28 23:25:38 +0000

    0-strcatfile

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+char *_strcat(char *dest, char *src)
+{
+	char *i = dest;
+	char *j = src;
+	
+	if (i > j)
+	{
+		_putchar(j > i);
+	}
+	else
+	{
+		_putchar('\0');
+	}
+	return (i);
+}
+	
