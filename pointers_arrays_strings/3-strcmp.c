1d40244b20042253f90b98d0b1442e4d82cd6c7f
Scopecr <wjrcarrion@gmail.com>
2023-06-29 18:39:39 +0000

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,24 +1,23 @@
 #include "main.h"
+#include <stdio.h>
+/**
+ * _strcmp - compares two strings
+ * @s1: input string 1
+ * @s2: input string 2
+ * Return: difference in ASCII value or 0 for true
+ */
 
 int _strcmp(char *s1, char *s2)
 {
+int i = 0;
 
-	while(*s1 != '\0' && *s1 == *s2)
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		s1++;
-		s2++;
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+
+		i++;
 	}
 
-	if (*s1 == *s2)
-	{
-		return (0);
-	}
-	else if (*s1 < *s2)
-	{
-		return (-1);
-	}
-	else
-	{
-		return (1);
-	}
+	return (0);
 }
