82c345134e6eb10dd4b0a3e7367e916218412a08
Scopecr <wjrcarrion@gmail.com>
2023-06-30 15:12:46 +0000

    0-memset.c

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_memset - function that fils memory with a constanst byte.
+ *
+ * @n: unsigned value
+ * @s: the address to fill n
+ * @b: the constant to fill n
+ *
+ * Return: s for success
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	while (n > s[i])
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
