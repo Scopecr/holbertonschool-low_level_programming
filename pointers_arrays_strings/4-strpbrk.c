a296580683ea73c87eeb2c8d295f004e1eca43d1
Scopecr <wjrcarrion@gmail.com>
2023-06-30 23:12:01 +0000

    4-strpbrk.c

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,12 +1,15 @@
-#include "main.h"
+#include <stdlib.h>
+#include "holberton.h"
 /**
- * _strpbrk - searches a string for any of a set of bytes
+ * _strpbrk - searchs a string for any of a set of bytes
  *
- * @s: the string to seatch
+ * @s: the string to search
  * @accept: the bytes to search for
  *
- * Return: a pointer to the byte in @s that matches one of the bytes in @accept
+ * Return: a pointer to the byte in @s that matches one of the bytes in
+ * @accept, or NULL if no such byte is found.
  */
+
 char *_strpbrk(char *s, char *accept)
 {
 	int i;
