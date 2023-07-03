ca85855adfa6819a410431eb2ba061c2c83f868d
Scopecr <wjrcarrion@gmail.com>
2023-07-03 21:28:57 +0000

    4-pow_recursion.c

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,11 +1,20 @@
 #include "main.h"
 /**
+ * _pow_recursion - function that elevates x to the power of y
+ *
+ * @y: variable as exponent
+ * @x: variable to be elevated by y
+ * Return: -1 if @y is lower than 0 and 1 if equal to 0
  */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
+	{
 		return (-1);
+	}
 	if (y == 0)
-		 return (1);
-	 return x * _pow_recursion(x, y - 1);
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
 }
