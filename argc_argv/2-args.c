f18bfcd19c3039653603ddfcf4886282a8bc746f
Scopecr <wjrcarrion@gmail.com>
2023-07-05 23:00:44 +0000

    2-args.c

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,21 +1,20 @@
 #include "main.h"
 /**
+ * main - function to print name of function followed by newline
+ * @argc: argument to determine how many arguments to prgram are provided
+ *
+ * @argv: argument array with pogram name and arguments
+ *
+ * Return: (0)
  */
 
 int main(int argc, char* argv[])
 {
 	int i;
 
-	if (argc == 1)
+	for (i = 0; i <=(argc - 1); i++)
 	{
-        printf("%d\n", argc - 1);
-    } 
-	else
-	{
-	for (i = 0; i < argc; i++)
-	{
-        printf("%s\n", argv[i]);
-    }
+		printf("%s\n", argv[i]);
 	}
 return (0);
 }
