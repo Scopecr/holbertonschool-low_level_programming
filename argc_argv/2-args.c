9327766cb2f01778d25d5d2adcc9cf8ac39b691b
Scopecr <wjrcarrion@gmail.com>
2023-07-05 23:01:53 +0000

    2-args.c

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -8,11 +8,11 @@
  * Return: (0)
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; i <=(argc - 1); i++)
+	for (i = 0; i <= (argc - 1); i++)
 	{
 		printf("%s\n", argv[i]);
 	}
