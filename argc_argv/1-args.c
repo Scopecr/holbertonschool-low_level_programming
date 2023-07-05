9596028319a0f4c75845d3f546e29a6cc3da2cd1
Scopecr <wjrcarrion@gmail.com>
2023-07-05 21:40:29 +0000

    1-args.c

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -11,10 +11,14 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; i < argc; i++)
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
 	{
-		printf("%s", argv[i]);
+		for (i = 0; *argv; i++, argv++)
+			;
+		
+		printf("%d\n", i - 1);
 	}
-	printf("\n");
-	return (0);
+		return (0);
 }
