fb583ca2d6b32f2be92e8c9c0bb1a7eb0cd272a5
Scopecr <wjrcarrion@gmail.com>
2023-07-05 16:54:26 +0000

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,14 +1,19 @@
 #include "main.h"
 
 /**
- */
+ * argc - has the file and name
+ * argv - i a array of strings
+ * @i: the varible used to count the charactrs on the string
+ *
+ * Return: 0 success
+*/
 int main(int argc, char *argv[])
 {
 	int i;
-	
+
 	for (i = 0; i < argc; i++)
 	{
-		printf("arvg[%s]", argv[i]);
+		_putchar('argv[%s]', argv[i]);
 	}
 	return (0);
 }
