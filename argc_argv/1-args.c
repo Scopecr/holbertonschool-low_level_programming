fe212dd86a981a0e8b7d04fd5e554d2df29ee35a
Scopecr <wjrcarrion@gmail.com>
2023-07-05 19:56:01 +0000

    updated betty

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,5 +1,10 @@
 #include "main.h"
 /**
+ * main - program that prints the number of argunments
+ * @argc: variable that holds the program name executable
+ * @argv: array that holds the string of program
+ * 
+ * Return: 0 success
  */
 
 int main(int argc, char *argv[])
@@ -8,7 +13,7 @@ int main(int argc, char *argv[])
 
 	for (i = 0; 1 < argc; i++)
 	{
-		printf("args[%d]\n", argv[i]);
+		printf("args[%s]\n", argv[i]);
 	}
 	return (0);
 }
