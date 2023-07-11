41a28a9007faa49a30b07dfd70ab17b483723390
Scopecr <wjrcarrion@gmail.com>
2023-07-11 00:11:12 +0000

    0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,10 +1,18 @@
 #include "main.h"
 #include <string.h>
+/**
+ * create_array - create the array to be printed
+ * malloc - returnd value to the char pointer
+ * @c: is the array to be printed
+ * @size: lenght of array
+ * Return: array for success
+ */
 
 char *create_array(unsigned int size, char c)
 {
 
 	char *array = (char *)malloc(sizeof(char) * size);
+
 	memset(array, c, size);
 	return (array);
 }
