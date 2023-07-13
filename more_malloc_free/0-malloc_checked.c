166c5da7e9158bb7565740ccf7d4e1986057ac1a
Scopecr <wjrcarrion@gmail.com>
2023-07-13 02:55:58 +0000

    0-malloc_checked.c

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,8 +1,15 @@
 #include "main.h"
 
+
+/**
+ * *malloc_checked - allocate memory with malloc
+ * @b: unsigned int type
+ * Return: return pointer
+ */
+
 void *malloc_checked(unsigned int b)
 {
-	
+
 	int *ptr;
 
 	ptr = malloc(b);
