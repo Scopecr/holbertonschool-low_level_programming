5b61af56ed2413f1ffaacb6f31d096e4cd8a16b5
Scopecr <wjrcarrion@gmail.com>
2023-07-15 00:35:18 +0000

    2-main.c

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main: function that is going to execute the file
+ * main - function that is going to execute the file
  *
  * Return: 0 void function
  */
 
 int main(void)
 {
-	printf("main.c");
+	printf("%s\n", __FILE__);
 	return (0);
 }
