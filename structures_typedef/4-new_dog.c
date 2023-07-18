6017b47f0db9c06c36060f957a8e6b59179fef21
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-07-18 15:35:09 -0400

    updated 4-newdog.c

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+* New_dog - function for creating a new dog
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
+* strcpy - copy the name of owner and dog
+* 
+*/
+dog_t *now_dog(char *name, float age, char *owner)
+{
+    dog->name = strcpy(name);
+    dog->age = age;
+    dog->owner = strcpy(owner);
+    if (dog <= 0)
+    {
+        return (NULL);
+    }
+}
