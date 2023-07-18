f23252f97b2e2779d1a9ad2295822756a09a51d5
Scopecr <wjrcarrion@gmail.com>
2023-07-18 17:10:28 +0000

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,16 +2,20 @@
 #define DOG
 /**
  * struct dog - definde the data types
+ * struct dog_t - second struct name
  * @name: string character
  * @age: float
  * @owner: strin character
  */
-struct dog
+struct dog struct dog_t
 {
 char *name;
 float age;
 char *owner;
-}
+};
+
+/*prototypes*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
