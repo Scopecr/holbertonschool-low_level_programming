a76ec7a991b71a23d4e37836f1a3be5d96c5b6fc
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-07-19 15:57:48 -0400

    5-free_dog

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,9 +1,24 @@
 #include "dog.h"
 /**
- * 
+ *free_dog function for free dog object
+ *
+ * @d: free dog object
+ *
+ * Return: void nothing
  */
 void free_dog(dog_t *d)
 {
-    dog_t malloc(d);
+    if (d == 0)
+    {
+        return;
+    }
+    if (d->name)
+    {
+        free(d->name);
+    }
+    if (d->owner)
+    {
+        free(d->owner);
+    }
     free(d);
 }
