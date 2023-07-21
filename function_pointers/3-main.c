ef950eb0df03ced37e8c1bd271b218ae86d1ecf4
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-07-21 16:44:14 -0400

    added opt_func

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -15,6 +15,9 @@ int main(int argc, char *argv[])
         printf("Error\n");
         exit(98);
     }
+
+    op_func = get_op_func(argv[2])
+    
     if (op_func == NULL)
     {
         printf("Error\n");
