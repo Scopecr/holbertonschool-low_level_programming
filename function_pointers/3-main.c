fd272c1aa65766d8db7faa7d4dbabbe186f86492
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-07-21 18:11:35 -0400

    updated main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,39 +1,36 @@
-#include "3-calc.h"
-
-
+#include "calc.h"
 /**
-*main function- iniciates the main program
-*@argc: counts the number of arguments
-*@argv: vector of strings
-*Rerturn - 0 success
-*/
-
+ * main - main function
+ * @argc: count
+ * @argv: arguments
+ *
+ * Return: (0)
+ */
 int main(int argc, char *argv[])
 {
-    int (*op_func)(int, int);
-    int n2;
+	int (*op_func)(int, int);
+	int n2;
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	op_func = get_op_func(argv[2]);
 
-    op_func = get_op_func(argv[2]);
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    if (op_func == NULL)
-    {
-        printf("Error\n");
-        exit(99);
-    }
-    
-    n2 = atoi(argv[3]);
+	n2 = atoi(argv[3]);
 	if ((atoi(argv[2]) == atoi("/") || atoi(argv[2]) == atoi("%")) && n2 == 0)
-    {
-        printf("Error\n");
-        exit(100);
+		{
+		printf("Error\n");
+		exit(100);
+		}
 
-    }
-    printf("%d\n", op-func(atoi(argv[1]), atoi(argv[3])));
-    return (0);
+	printf("%d\n", op_func(atoi(argv[1]), atoi(argv[3])));
+	return (0);
 }
