e84944682f8038a44125905ef5bdf04aab28fb47
Scopecr <wjrcarrion@gmail.com>
2023-07-22 02:23:09 +0000

    3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,52 +1,80 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
+/**
+ * prchr - prints chars
+ * @args: pointer to args
+ */
+void prchr(va_list *args)
+{
+	printf("%c", va_arg(*args, int));
+}
+/**
+ * print - prints ints
+ * @args: pointer to args
+ */
+void print(va_list *args)
+{
+	printf("%d", va_arg(*args, int));
+}
+/**
+ * prflt - prints floats
+ * @args: pointer to args
+ */
+void prflt(va_list *args)
+{
+	printf("%f", va_arg(*args, double));
+}
+/**
+ * prstr - prints strings
+ * @args: pointer to args
+ */
+void prstr(va_list *args)
+{
+	char *ar;
 
-
+	ar = va_arg(*args, char *);
+	if (ar == NULL)
+		ar = "(nil)";
+	printf("%s", ar);
+}
+/**
+ * print_all - prints everything
+ * @format: const format string
+ *
+ * Return: void
+ */
 void print_all(const char * const format, ...)
 {
+	int i = 0, j = 0;
+	char *sep = "";
 	va_list args;
+	typ types[] = {
+		{"c", prchr},
+		{"i", print},
+		{"f", prflt},
+		{"s", prstr},
+		{NULL, NULL}
+	};
+
 	va_start(args, format);
-
-	char c;
-	char *s_arg;
-
-
-	while (*format)
+	while (format != NULL && format[i] != '\0')
 	{
-		c = *format;
-
-		if (c == 'c')
+		j = 0;
+		while (types[j].type != NULL && *(types[j].ltr) != format[i])
 		{
-			putchar(va_arg(args, int));
+			j++;
 		}
-		else if (c == 'i')
+		if (types[j].type != NULL)
 		{
-			printf("%d", va_arg(args, int));
+			printf("%s", sep);
+			types[j].type(&args);
+			sep = ", ";
 		}
-		else if (c == 'f')
-		{
-			printf("%f", va_arg(args, double));
-		}
-		else if (c == 's')
-		{
-			s_arg = va_arg(args, char *);
-
-			if (s_arg == NULL)
-			{
-				printf("(nil)");
-			}
-			else
-			{
-				printf("%s", s_arg);
-			}
-		}
-
-		format++;
+		i++;
 	}
-
 	va_end(args);
-
-	putchar('\n');
+	printf("\n");
 }
+
 
