d880fd976c8285eff8d811ada334d59fdf0b2b92
Scopecr <wjrcarrion@gmail.com>
2023-07-22 02:21:01 +0000

    3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,88 +1,52 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
-/**
- * print_all - print anything
- * @format: constant
- */
+
+
 void print_all(const char * const format, ...)
 {
-	unsigned int i = 0;
-	unsigned int j = 0;
-	char *separator = "";
 	va_list args;
-
-	f_t f_types[] = {
-		{"c", print_char},
-			{"i", print_int},
-			{"f", print_f},
-			{"s", print_str},
-			{NULL, NULL}
-	};
 	va_start(args, format);
 
-	while (format != NULL && format[i])
-	{
-		j = 0;
+	char c;
+	char *s_arg;
 
-		while (j < 4)
+
+	while (*format)
+	{
+		c = *format;
+
+		if (c == 'c')
 		{
-			if (format[i] == *f_types[j].id)
-			{
-				f_types[j].f(separator, args);
-				separator = ", ";
-			}
-			j++;
+			putchar(va_arg(args, int));
 		}
-		i++;
+		else if (c == 'i')
+		{
+			printf("%d", va_arg(args, int));
+		}
+		else if (c == 'f')
+		{
+			printf("%f", va_arg(args, double));
+		}
+		else if (c == 's')
+		{
+			s_arg = va_arg(args, char *);
+
+			if (s_arg == NULL)
+			{
+				printf("(nil)");
+			}
+			else
+			{
+				printf("%s", s_arg);
+			}
+		}
+
+		format++;
 	}
+
 	va_end(args);
-	printf("\n");
+
+	putchar('\n');
 }
 
-/**
- * print_char - print anything
- * @separator: space
- * @args: arguments
- */
-void print_char(char *separator, va_list args)
-{
-	printf("%s%c", separator, va_arg(args, int));
-}
-
-/**
- * print_int - print anything
- * @separator: space
- * @args: arguments
- */
-void print_int(char *separator, va_list args)
-{
-	printf("%s%i", separator, va_arg(args, int));
-}
-
-/**
- * print_f - print anything
- * @separator: space
- * @args: arguments
- */
-void print_f(char *separator, va_list args)
-{
-	printf("%s%f", separator, va_arg(args, double));
-}
-
-/**
- * print_str - print anything
- * @separator: space
- * @args: arguments
- */
-void print_str(char *separator, va_list args)
-{
-	char *arg = va_arg(args, char *);
-
-	if (arg == NULL)
-	{
-		printf("%s%s", separator, "(nil)");
-		return;
-	}
-	printf("%s%s", separator, arg);
-}
