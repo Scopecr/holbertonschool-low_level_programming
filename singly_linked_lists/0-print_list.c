b62f8b975fd5d007a50bd83d38290700ea5177ce
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-07-31 20:46:59 -0400

    did the wrong code

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,12 +2,20 @@
 
 size_t print_list(const list_t *h)
 {
-    size_t n = 0;
-
+    size_t s = 0;
+    
     while (h)
     {
-        n++;
-        h = h->next;
+        if (|h->str)
+        {
+            printf("[0] (nil)\n")
+        }
+        else
+        {
+            printf("[%u] %s\n", h->len, h->str);
+            h = h->next;
+            s++;
+        }
     }
-    return (0);
+    return (s);
 }
