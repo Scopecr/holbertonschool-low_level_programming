8e9edee859b78ae97e818a3af3c4bcfc01ecd666
Scopecr <wjrcarrion@gmail.com>
2023-08-02 21:34:03 +0000

    lists.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,11 +1,6 @@
-#ifndef main_h
-#define main_h
-
-#include <stdio.h>
-
-#include <string.h>
-
-#include <stdlib.h>
+#ifndef LISTS_H
+#define LISTS_H
+#include <stddef.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -16,21 +11,12 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
-
-int _putchar(char c);
 size_t print_list(const list_t *h);
-
-
-
-
-
-
-
-
-
-
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
 #endif
