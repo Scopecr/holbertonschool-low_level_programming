3dffee7a258264ca0e56fb99d65fffc6b3c8409e
Scopecr <wjrcarrion@gmail.com>
2023-08-02 23:55:40 +0000

    2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /*
- *add_node - adds new node to lists
+ **add_node - adds new node to lists
  *@head: a pointer to a pointer
  *@str: pointer to contant character
  *Return: address of the new element null
@@ -9,8 +9,10 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = (list_t *)malloc(sizeof(list_t));
+
 	if (new_node == NULL || str == NULL)
-		return(NULL);
+
+		return (NULL);
 	new_node->str = strdup(str);
 	new_node->len = strlen(str);
 	new_node->next = *head;
