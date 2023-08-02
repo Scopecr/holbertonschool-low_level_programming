b49643de201edf18b58bf1ba8b801b8b739d8a16
Scopecr <wjrcarrion@gmail.com>
2023-08-02 23:43:39 +0000

    2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -9,10 +9,8 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	lists_t *new_node = (lists_t *)malloc(sizeof(list_t));
-	if (new_node == NULL)
-	{
+	if (new_node == NULL || str == NULL)
 		return(NULL);
-	}
 	new_node->str = strdup(str);
 	new_node->len = strlen(str);
 	new_node->next = *head;
