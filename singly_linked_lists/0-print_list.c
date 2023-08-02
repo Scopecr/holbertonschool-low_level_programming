e8ba9919a9d8daee74e77420b0ea0f120eaeedc8
Scopecr <wjrcarrion@gmail.com>
2023-08-02 14:37:44 +0000

    0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <stdio.h>
+
 /**
  * print_list - prints all the list of the string
  * @h: pointer to the list_t list to point
@@ -9,13 +11,16 @@ size_t print_list(const list_t *h)
 {
 	size_t s = 0;
 
-	while (h)
+	while(h != NULL)
 	{
-		if (!h->str)
+		if (str == NULL)
+		{
 			printf("[0] (nill)\n");
+		}
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
+		s++;
 	}
 	return (s);
 }
