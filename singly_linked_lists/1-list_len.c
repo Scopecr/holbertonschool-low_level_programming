4ffe4ac3e2ed4c10eccbd916348c97f550d0d9f8
Scopecr <wjrcarrion@gmail.com>
2023-08-02 21:27:23 +0000

    1-list_len.c

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -9,12 +9,15 @@
 
 size_t list_len(const list_t *h)
 {
-	size_t n = 0;
+	size_t count = 0;
 
-	while(h)
+	while(h = != NULL)
 	{
-		n++;
+		if (h-> != NULL)
+		{
+			count++;
+		}
 		h = h->next;
 	}
-	return (n);
-)
+	return (count);
+}
