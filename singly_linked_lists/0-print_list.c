d0730dc904b2d8d75fa7fc508c8fa7a44fc94bb6
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-08-02 09:24:48 -0400

    indent

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -13,13 +13,12 @@ size_t print_list(const list_t *h)
 	{
 		if (h == NULL)
 		{
-			printf("[0] (nill)\n");
+            printf("[0] (nill)\n");
 		}
 		else
-			printf("[%u] %s\n", h->len, h->str);
+		printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		s++;
 	}
 	return (s);
 }
-
