1011aaf4a23673700915d0a479a08f6fd6193e3b
Scopecr <wjrcarrion@gmail.com>
2023-08-03 00:16:15 +0000

    3-add_node_end.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -26,6 +26,6 @@ list_t *add_node_end(list_t **head, const char *str)
 	temp = *head;
 	while (temp->next != NULL)
 		temp = temp->next;
-	temp->next = new_tail;
+	temp->next = b_node;
 	return (temp);
 }
