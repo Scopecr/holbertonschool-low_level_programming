0683c9e9b05cd7f0a2750f4208c7f9e851f6b545
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-08-04 16:41:11 -0400

    4-free_dlistint.c

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h""
+/**
+ * freelist - frees the dlistint_t
+ * 
+ * @head: list given to free
+ * 
+ * Return: 
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *freelist;
+
+	while (head)
+	{
+		freelist = head->next;
+		free head->prev;
+		free head->next;
+		head = freelist;
+	}
+}
