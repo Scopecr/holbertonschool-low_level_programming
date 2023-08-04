155f69936472139c583ef29327765ce033d2688d
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-08-04 14:04:59 -0400

    indented

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -7,29 +7,29 @@
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *enode = malloc(sizeof(dlistint_t));
+	dlistint_t *enode = malloc(sizeof(dlistint_t))
 
 	if (enode == NULL)
-		return (NULL);
+	return (NULL);
 
-    enode->n = n;
-    enode->next = NULL;
-    
-    if (*head == NULL)
-    {
-        enode->prev = NULL;
-        *head = enode;
-    }
-    else
-    {
-        dlistint_t *current = *head;
-        while (current->next != NULL)
-        {
-            current = current->next;
-        }
-        current->next = enode;
-        enode->prev = current;
-    }
+	enode->n = n;
+	enode->next = NULL;
+
+	if (*head == NULL)
+	{
+		enode->prev = NULL;
+		*head = enode;
+	}
+	else
+	{
+	dlistint_t *current = *head;
+	while (current->next != NULL)
+	{
+		 current = current->next;
+	}
+		current->next = enode;
+		enode->prev = current;
+	}
 
 	return (enode);
 }
