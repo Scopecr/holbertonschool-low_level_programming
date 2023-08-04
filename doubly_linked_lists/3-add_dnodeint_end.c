852c3d85ce6aab5bd5874395d2c9949618fe1914
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-08-04 12:13:30 -0400

    3-add_dnodeint_end.c

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -3,33 +3,33 @@
  * add_dnodeint_end - adds end node to the dlist
  * @enode is the ponter to new node
  * Return: 1 if error enode if success
-*/
+ */
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *enode = malloc(sizeof(dlistint_t));
+	dlistint_t *enode = malloc(sizeof(dlistint_t));
 
-    if (enode == NULL)
-    return (NULL);
+	if (enode == NULL)
+		return (NULL);
 
-    enode->n = n;
-    enode->next = NULL;
-    
-    if (*head == NULL)
-    {
-        enode->prev = NULL;
-        *head = enode;
-    }
-    else
-    {
-        dlistint_t *current = *head;
-        while (current->next != NULL)
-        {
-            current = current->next;
-        }
-        current->next = enode;
-        enode->prev = current;
-    }
+	enode->n = n;
+	enode->next = NULL;
 
-    return (enode);
+	if (*head == NULL)
+	{
+		enode->prev = NULL;
+		*head = enode;
+	}
+	else
+	{
+		dlistint_t *current = *head;
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = enode;
+		enode->prev = current;
+	}
+
+	return (enode);
 }
