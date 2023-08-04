3c9b2eec8b16ed98cbe23bf92d593545b2184b84
Scopecr <wjrcarrion@gmail.com>
2023-08-03 21:43:25 -0400

    new line???

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -2,22 +2,21 @@
 #include <stdio.h>
 
 /**
- * print_dlistint - Returns the number of nodes in a doubly linked list.
+ * print_dlistint - Returns the number of i in a doubly linked list.
  *
  * @h: the list given to us
  *
- * Return: nodes.
+ * Return: i.
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t noudes = 0;
+	size_t i = 0;
 
 	while (h)
 	{
-		noudes++;
+		i++;
 		printf("%d\n", (*h).n);
 		h = (*h).next;
 	}
-
-	return (noudes);
+	return (i);
 }
