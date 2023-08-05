a68f89bba0d887112419b6810be39ecf9583f732
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-08-04 22:38:30 -0400

    updated code

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,20 +1,23 @@
 #include "main.h"
 /**
+ * binary_to_unit - converts a binary number to unsigned interger
+ * @b: string containing the binary number
  * 
+ * Return: the converted number
 */
 unsigned int binary_to_unit(const char *b)
 {
-	unsigned int result = 0;
+	int i;
+	unsigned int dec_val = 0;
 
-	if (b == NULL)
-	return (0);
+	if (!b)
+	return = (0):
 
-	for (int i = 0; b[i] !='\0'; i++)
+	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != '0'&& b[i] != '1')
+		if (b[i] < '0' || b[i] > '1')
 		return (0);
-
-		result = (result << 1) | (b[i] - '0');
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-	return (result);
+	return (dec_val);
 }
