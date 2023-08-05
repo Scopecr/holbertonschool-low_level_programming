470827511212f4cf5e47554b388e8d138275fa0c
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-08-04 20:56:38 -0400

    get dnodeint

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -2,7 +2,8 @@
 /**
  * get_dnodeint_at_index - function that gets the nth node
  * @i: pointer to the head pointer
- *
+ * @index: unsigned interger
+ * @head: pointer of list 
  * Returns: NULL if node does not exist
 */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
