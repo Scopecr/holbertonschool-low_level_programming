f6123d0e361a753c5528b89cae4ff679fb255c37
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-08-04 20:04:49 -0400

    5-get_dnode

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - function that gets the nth node
+ * @i: pointer to the head pointer
+ * @count: vatiable to count how many nodes i have
+ *
+ * Returns: NULL if node does not exist
+*/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *i = head;
+	unsigned int count = 0;
+
+	while (count != NULL)
+	{
+		if(count == index)
+		return i;
+
+		count++;
+		i = i->next;
+	}
+	return (NULL);
+}
