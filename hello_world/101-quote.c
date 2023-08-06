94360efa6de9872f97211eaf64a2327da4b79606
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-08-05 21:02:03 -0400

    fixed syntax

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,12 +1,10 @@
-#include<stdio.h>
-/**
- * main- entery point
- *
- * Return: always 1 (Success)
- */
-int main(void) 
-{
-	putchar('and that piece of art is useful\" %c Dora Kopar, 2015%c20%c19\n', 45);
-	return (1);
-}
+#include <unistd.h>
 
+int main()
+{
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, message, sizeof(message) - 1);
+
+	return 1;
+}
