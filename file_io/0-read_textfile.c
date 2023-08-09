b0fde405df394add9d5500ccbf004ec3bd704df2
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-08-09 19:40:13 -0400

    main.h and 0-read_textfile

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stslib.h>
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	/**
+	 * read_textfile - reads filename from STDOUT
+	 * @filename: file to be read
+	 * @letters: number of letters to be read
+	 * Return: w - actual number of bytes read and printed
+	 * 	0 when function fails or filename is NULL
+	*/
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
+
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	buf = malloc(siezof(char)*letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, but, t);
+
+	free(buf);
+	close(fd);
+	return(w);
+}
