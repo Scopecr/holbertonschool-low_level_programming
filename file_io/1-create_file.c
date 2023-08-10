833e94ad2a206a8a6517bc37192a349e728499eb
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-08-09 22:04:04 -0400

    betty

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,34 +1,34 @@
 #include "main.h"
 /**
  * create_file - creates file
- * @filename: A pointer to the name of the file to create
- * @text_content: A pointer to the name of the file create
+ * @filename: pointer to file to be created
+ * @text_content: pointer to content to be written
+ * Return: -1 if NULL or fails 
+ * 	1 if complete
  * 
- * Return: if function fails - - 1
- * 		Otherwise -1
 */
-
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, len = 0;
-
 	if (filename == NULL)
 	{
-		return (-1);
+		return -1;
 	}
-
+	int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+	{
+	return -1;
+	}
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-		len++;
-	}
-	if (fd == -1 || w == -1)
-	{
-		return (-1);
+		int w = write(fd, text_content, strlen(text_content));
+		if (w == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 
 	close(fd);
 
 	return (1);
 }
-
