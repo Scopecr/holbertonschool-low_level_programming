4475fddb9eb60cece1902e170b8ab8adb3200f1e
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-08-09 22:43:42 -0400

    added len for fix

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -9,7 +9,7 @@
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, w, len = 0;
 
 	if (filename == NULL)
 	{
@@ -17,14 +17,16 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd < 0)
-		return (-1);
-	if (text_content == NULL)
+	w = write(fd, text_content, len);
+	
+	if (text_content != NULL)
 	{
-		close(fd);
-		return (1);
+		for (len = 0; text_content[len];)
+		{
+			len++;
+		}
 	}
-	else
+	if (fd == -1 || w == -1)
 	{
 		return (-1);
 	}
