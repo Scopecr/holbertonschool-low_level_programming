30e80e53ecd620f316fd7324da8f3b295848fc46
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-08-09 22:51:27 -0400

    rearrage

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -25,13 +25,13 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	fd = open(filename, O_RDWR | O_APPEND);
-
+	w = write(fd, text_content, len);
+	
 	if (fd == -1 || w == -1)
 	{
 		return (-1);
 	}
 	
-	w = write(fd, text_content, len);
 	close(fd);
 	if (w < 0)
 	{
