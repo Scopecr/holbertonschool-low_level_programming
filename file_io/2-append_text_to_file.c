e70ee15b898807dbce159fbe3f9d2421dfa50f9d
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-08-09 22:28:58 -0400

    added equal

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -17,7 +17,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	fd = open(filename, O_WRONLY | O_APPEND);
-	if (text_content = NULL)
+	if (text_content == NULL)
 	{
 		close(fd);
 		return (1);
