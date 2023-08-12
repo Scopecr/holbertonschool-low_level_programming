2720bb73feb14e0a71c62ea38ec92e395d3fbc75
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-08-11 20:07:50 -0400

    0-hash

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -3,7 +3,7 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t = *new_h;
+	hash_table_t  *new_h;
 
 	new_h = malloc(sizeof(hash_table_t));
 	if (new_h == NULL)
--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,5 +1,9 @@
 #ifndef HASHT
 #define HASHT
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * struct hash_node_s - Node of a hash table
  *
