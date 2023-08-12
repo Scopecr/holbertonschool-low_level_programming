45e939bcfdad943978e142a2112037cadf25b426
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-08-11 20:12:04 -0400

    description

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_create - creates a hash table.
+ *
+ * @size: size of a hashtable
+ *
+ * Return: hash table strings when added.
+ */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
