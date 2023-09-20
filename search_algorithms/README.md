bc37618968b05b97b37598d81c9d19734eff1ec3
Scopecr <wjrcarrion@gmail.com>
2023-09-20 23:02:42 +0000

    created readme .h file and 0-linear.c

---
+++ search_algorithms/README.md
@@ -0,0 +1,1 @@
+this read me is for the search algorithms
---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,7 @@
+#ifdef _SEARCHALGO_
+#define _SEARCHALGO_
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
