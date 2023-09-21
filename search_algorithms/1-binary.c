0e91f0558d578444da7a332119fbc0400c79987a
Scopecr <wjrcarrion@gmail.com>
2023-09-21 16:45:02 -0400

    updated code fingers crossed

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -10,46 +10,30 @@ void print_array(int *array, size_t i, size_t size);
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t low = 0, high = size - 1, half = 0;
+size_t left = 0, right = size - 1, mid = 0, i = 0;
 
-	if (array == NULL || value == 0)
+	if (array == NULL)
 		return (-1);
 
-	print_array(array, low, size);
-	while (low < high)
+	while (left <= right)
 	{
-		half = (low + high) / 2;
-		if (array[half] < value)
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
 		{
-			low = half + 1;
-			print_array(array, low, high + 1);
+			printf("%d", array[i]);
+			if (i < right)
+				printf(", ");
 		}
-		else if (array[half] > value)
-		{
-			high = half - 1;
-			print_array(array, low, high + 1);
-		}
+		printf("\n");
+
+		mid = (left + right) / 2;
+
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			left = mid + 1;
 		else
-			return (array[half]);
+			right = mid - 1;
 	}
 	return (-1);
-}
-
-/**
- * print_array - print all elements in an array
- * @array: list of elements
- * @i: index to init
- * @size: size of the array
- */
-void print_array(int *array, size_t i, size_t size)
-{
-	printf("Searching in array: ");
-	while (i < size)
-	{
-		if (i + 1 == size)
-			printf("%d ", array[i]);
-		else
-			printf("%d, ", array[i]);
-		i++;
-	} printf("\n");
 }
